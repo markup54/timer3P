8ddbed2c40835bdec406a4dd9aba4b71293aa422
MarzenaK <marzenakupka@tlen.pl>
2021-11-18 11:48:10 +0100

    przyciski dzialaja

--- app/src/main/java/pl/example/timer3p/MainActivity.java
+++ app/src/main/java/pl/example/timer3p/MainActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int sekundy=0;
-    private TextView textView;
+    private String tekst;
+    private TextView textView, zapisTextView;
     private boolean zlicza = false;
     private Button startButton,stopButton, resetButton, zapiszButton;
     @Override
@@ -18,8 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.textview);
+        zapisTextView = (TextView) findViewById(R.id.textview2);
         runTimer();
+        startButton = (Button) findViewById(R.id.button);
+        stopButton = (Button) findViewById(R.id.button2);
+        resetButton = (Button) findViewById(R.id.button3);
+        zapiszButton = (Button) findViewById(R.id.button4);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zlicza =true;
+            }
+        });
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zlicza = false;
+            }
+        });
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sekundy=0;
+            }
+        });
+        zapiszButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String zapisane = zapisTextView.getText().toString();
+                zapisane = zapisane +"\n"+tekst;
+                zapisTextView.setText(zapisane);
+            }
+        });
     }
+
+
 
     private void runTimer(){
         final Handler handler = new Handler();
@@ -29,7 +65,11 @@ public class MainActivity extends AppCompatActivity {
                 if(zlicza) {
                     sekundy++;
                 }
-                textView.setText(Integer.toString(sekundy));
+                int s = sekundy%60;
+                int m = (sekundy/60)%60;
+                int h = sekundy/3600;
+                tekst = String.format("%d:%02d:%02d",h,m,s);
+                textView.setText(tekst);
                 handler.postDelayed(this,1000);
             }
         });
